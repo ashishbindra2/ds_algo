735c473f413b7c0d86552f5603254736a5736675
Ashish bindra <ashishbindra2@gmail.com>
2020-08-05 14:00:49 +0530

    literals

---
+++ programs/Mixed_Mode_operation.java
@@ -0,0 +1,20 @@
+package programs;
+
+public class Mixed_Mode_operation {
+
+	public static void main(String[] args) {
+		// Java program to illustrate the behaviour of 
+		// char literals and integer literals when 
+		// we are performing addition 
+		 
+		        // ASCII value of 0 is 48 
+		        int first = '0'; 
+		  
+		        // ASCII value of 7 is 55 
+		        int second = '7'; 
+		        System.out.println("Ashish" + first + 
+		                                '2' + second); 
+
+	}
+
+}
