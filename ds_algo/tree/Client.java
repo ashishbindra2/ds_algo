945bbe70938d2bcbee49ccc98aa51ddbf2cbfefd
Ashish bindra <ashishbindra2@gmail.com>
2021-03-04 11:07:41 +0530

    tree

---
+++ ds_algo/tree/Client.java
@@ -0,0 +1,23 @@
+package tree;
+
+public class Client {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+        LinkedList ls = new LinkedList();
+        ls.display();
+        ls.addFirst(10);
+        ls.display();
+        ls.addLast(20);
+        ls.display();
+        ls.addFirst(20);
+        ls.display();
+        System.out.println(ls.getAt(1));
+        System.out.println(ls.getFirst());
+        System.out.println(ls.removeFirst());
+        ls.display();
+        ls.reversData();
+        ls.display();
+	}
+
+}
