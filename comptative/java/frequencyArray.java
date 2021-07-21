296d336baf7e9f35bb74383c04a48df4dcd8da50
Bindra <ashishbindra2@gmail.com>
2021-07-21 16:45:38 +0530

    Create frequencyArray.java

---
+++ comptative/java/frequencyArray.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Frequency {
+    public static void mapp(String s) {
+    	HashMap<Character, Integer> map = new HashMap<>();// Creating HashMap
+		char c;
+		for (int i = 0; i < s.length(); i++) {
+			c = s.charAt(i);
+			if (map.containsKey(c)) {
+				map.put(c, map.get(c) + 1);
+			} else {
+				map.put(c, 1);
+			}
+		}
+		System.out.println(map);
+
+    }
+    public static void frq(String s) {
+    	int frq[] = new int[256];
+    	for(char it:s.toCharArray()) {
+    		frq[it]++;
+    	}
+    	for(int i=0;i<256;i++) {
+    	
+    		if(frq[i]>0) {
+    			char c=((char)(frq[i]));
+    			System.out.println((char)i+"->"+frq[i]+"");
+    		}
+    		
+    	}
+    }
+	public static void main(String args[]) {
+		String s = "Aashish";
+		mapp(s);
+		frq(s);
+		
+	}
+}
