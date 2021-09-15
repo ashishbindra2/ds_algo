2c336f1c8b6a938802366682a058c03d817f921d
Bindra <ashishbindra2@gmail.com>
2021-09-15 12:43:34 +0530

    Update queres.sql

--- SQL/queres.sql
+++ SQL/queres.sql
@@ -1,3 +1,11 @@
-Q) 2nd Highest Salary
+Q1) 2nd(Secound) Highest Salary
  1) Approch SELECT MAX(Price) FROM [Products] WHERE Price < (SELECT MAX(Price) FROM [Products] );
  2) Approch SELECT MAX(Price) FROM [Products] WHERE Price NOT IN (SELECT MAX(Price) FROM [Products] );
+
+Q2) Department wise highest Salary or OrderId wise highest Salary.
+ 1) SELECT MAX(Quantity),OrderID FROM [OrderDetails] GROUP BY OrderID
+ 2) SELECT MAX(sal),deptNo FROM [emp] GROUP BY deptNo
+ 
+Q3) Counts No of EMployes ineach Department or counts no of Quantity in Each OrderID
+ 1) SELECT COUNT(*),OrderID FROM [OrderDetails] GROUP BY OrderID
+ 2) SELECT COUNT(*),deptNo FROM [emp] GROUP BY deptNo
