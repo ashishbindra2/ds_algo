45ca846d99fb0bf1b12a6e02e9918b1e50867d6b
Bindra <ashishbindra2@gmail.com>
2021-09-15 14:37:52 +0530

    Update queres.sql

--- SQL/queres.sql
+++ SQL/queres.sql
@@ -11,5 +11,31 @@ Q3) Counts No of EMployes ineach Department or counts no of Quantity in Each Ord
  1) SELECT COUNT(*),OrderID FROM [OrderDetails] GROUP BY OrderID
  2) SELECT COUNT(*),deptNo FROM [emp] GROUP BY deptNo
  
+Q4) Display Alternate Records( 1,3,5 or 2,4,6)?
+ 1) SELECT * FROM (SELECT ROWNUM rn,EMPNO,SAL,ENAME,DEPTNO FROM emp ORDER BY rn) WHERE MOD(rn,2)<>0;
+ 2) SELECT * FROM (SELECT ROWNUM rn,EMPNO,SAL,ENAME,DEPTNO FROM emp ORDER BY rn) WHERE MOD(rn,2)!=0;
+ 3) SELECT * FROM (SELECT ROWNUM rn,EMPNO,SAL,ENAME,DEPTNO FROM emp ORDER BY rn) WHERE MOD(rn,2) =0;
+ 
+Q5) Add numberas add view sal in desending order
+ 1) SELECT ROWNUM rn,EMPNO,SAL,ENAME,DEPTNO FROM emp ORDER BY SAL DESC;
+ 
+Q6) 
+Q7)
+Q8)
+Q9)
+Q10)
+Q11)
+Q12)
+Q14)
+Q15)
+Q16)
+Q17)
+Q18)
+Q19)
+Q20)
+Q21)
+Q22)
+Q23)
+Q24)
+Q25)
 
- Display Alternate Records
